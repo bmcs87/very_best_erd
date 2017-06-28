bb7a0fb6a657a0596f701642111386619bd7d038
Raghu Betina <raghu@firstdraft.com>
2017-06-28 02:14:11 +0000

    Whitelist columns in MenuItem for ActiveAdmin

--- app/admin/menu_item.rb
+++ app/admin/menu_item.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register MenuItem do
 
+ permit_params :dishes_id, :venues_id, :favorites_id, :name, :calories
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
