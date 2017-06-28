d2cc41ba9ca81963af3c2959ca04f8d2d6576d9b
Raghu Betina <raghu@firstdraft.com>
2017-06-28 02:13:40 +0000

    Direct association between neighborhoods and venues

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,6 +1,10 @@
 class Neighborhood < ApplicationRecord
   # Direct associations
 
+  has_many   :venues,
+             :foreign_key => "neighborhoods_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,9 @@
 class Venue < ApplicationRecord
   # Direct associations
 
+  belongs_to :neighborhoods,
+             :class_name => "Neighborhood"
+
   belongs_to :cuisines,
              :class_name => "Cuisine"
 
