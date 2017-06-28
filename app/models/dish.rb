e35f243c6e3dd3be1f17e74e00294f482ce85e9a
Raghu Betina <raghu@firstdraft.com>
2017-06-28 02:13:40 +0000

    Direct association between dishes and menu_items

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,10 @@
 class Dish < ApplicationRecord
   # Direct associations
 
+  has_many   :menu_items,
+             :foreign_key => "dishes_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,6 +1,9 @@
 class MenuItem < ApplicationRecord
   # Direct associations
 
+  belongs_to :dishes,
+             :class_name => "Dish"
+
   belongs_to :venues,
              :class_name => "Venue"
 
